e8f4136aaa06cd28342585dd47ca51b2764222ff
dhanibaksh777-byte <tera@email.com>
2026-08-04 02:16:15 +0500

    modified main.py and added frontend url

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from database import engine,Base
 from Routers import auth,tasks
 import models
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,6 +15,13 @@ app = FastAPI(
 
 app.include_router(auth.router)
 app.include_router(tasks.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://frontend-gse2.vercel.app/"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
