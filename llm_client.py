4f419149b53d59b058c18c24a50b63fc75732eed
dhanibaksh777-byte <dhanibaksh781@gmail.com>
2026-08-15 12:16:40 +0500

    changes the model name in llm

--- llm_client.py
+++ llm_client.py
@@ -32,7 +32,7 @@ def get_response(user_content: str):
     
     try:
         response1 = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model="openai/gpt-oss-120b",
             messages=[
                 {"role": "system", "content": system_prompt_with_date},
                 {"role": "user", "content": user_content}
